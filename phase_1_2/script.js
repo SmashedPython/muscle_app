4752dc4d2c38d766b13a611a157470e1936caf68
nveeragandham <nehaveeragandham26@gmail.com>
2025-03-02 17:48:08 -0500

    Profile tab and profile pic

--- phase_1_2/script.js
+++ phase_1_2/script.js
@@ -1,26 +1,30 @@
 function openCity(cityName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
     }
   
-    // Show the specific tab content
     document.getElementById(cityName).style.display = "block";
   
-    // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 }
   
-  // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+document.getElementById("profile-pic").addEventListener("click", function () {
+  document.getElementById("alert").style.display = "block";
+});
+
+function closeAlert() {
+  document.getElementById("alert").style.display = "none";
+}
 
 function printChoices() {
     const cameraType1 = document.querySelector('input[name="cameraType1"]:checked').value;
